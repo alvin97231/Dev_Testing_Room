669d3a4ca7edf567af40e64121ffad214bd06c12
alvin97231 <alvin.eustache@gmail.com>
2017-02-15 10:34:06 +0100

    update to bug report app

--- gulp/wiredep.js
+++ gulp/wiredep.js
@@ -4,10 +4,12 @@ var gulp = require('gulp');
 
 var wiredep = require('wiredep').stream;
 
+var clip = require('gulp-clip-empty-files');
+
 // inject bower components
 gulp.task('wiredep', function () {
   return gulp.src('app/*.html')
-  
+    .pipe(clip())
     .pipe(wiredep({
       directory: 'app/bower_components'
     }))
